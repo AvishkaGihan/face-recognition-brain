80285b2f151eeb7820039d82ffa04ae966a00294
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2023-10-15 22:05:01 +0530

    main ui developed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import ParticlesBg from "particles-bg";
+
+import Navigation from "./Components/Navigation/Navigation";
+import Logo from "./Components/Logo/Logo";
+import Rank from "./Components/Rank/Rank";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+// import FaceRecognition from "./FaceRecognition";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
+      <FaceRecognition /> */}
+      </div>
+
+      <ParticlesBg type="cobweb" num={120} bg={true} color="#000000" />
+    </>
   );
 }
 
