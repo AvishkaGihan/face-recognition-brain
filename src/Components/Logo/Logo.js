7c1372a3ec3b09501a21289f5f0ea9551fdddc28
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2023-10-21 02:16:20 +0530

    updating tilt package code

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -7,12 +7,13 @@ import brain from "./brain.png";
 
 const Logo = () => {
   return (
-    <Tilt
-      className="pa3 Tilt br2 shadow-2 ma2"
-      style={{ height: 150, width: 150 }}
-    >
-      <img style={{ padding: "5px" }} src={brain} alt="logo" />
-    </Tilt>
+    <div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
+        <div className="pa3">
+          <img style={{ paddingTop: "5px" }} src={brain} alt="logo" />
+        </div>
+      </Tilt>
+    </div>
   );
 };
 
