7896244c88cbb8a4deacec49e437531c9cec40e2
AvishkaGihan <avishkag18@gmail.com>
2023-12-05 00:10:18 +0530

    Update API endpoints to use HTTPS

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
 
-    fetch("http://localhost:3000/imageUrl", {
+    fetch("https://face-recognition-brain-api-c8bf.onrender.com/imageUrl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -98,7 +98,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://face-recognition-brain-api-c8bf.onrender.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
